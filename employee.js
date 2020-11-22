51f99c1972976867de0551deecdff953e1f51eea
Benjamin Shostak <benshostak@rocketmail.com>
2020-11-21 21:03:59 -0800

    adding engineer and manager pages

--- employee.js
+++ employee.js
@@ -1,17 +1,22 @@
 // todo:write code to define and export the employee class
-const Employee = require("/employee.js");
+const Employee = require("../employee.js");
 
 class Employee  {
     constructor(name, id , email, getname){
         this.name = name;
         this.id = id;
         this.email = email;
-        this.getname = function(name) {
+        this.getName = function(name) {
             this.firstname = name;
         }
-        this.getid = function(id) {
+        this.getId = function(id) {
             this.id = id;
         }
-}
-
-module.exports = Employee;
+        this.getEmail = function(email) {
+            this.email = email;
+        }
+        this.getRole = function(Employee) {
+            this.role = Employee; 
+        }
+        module.exports = Employee;
+}};
