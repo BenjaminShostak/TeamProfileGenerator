7017f2acc798fc8ffc498c43ed2a6d5e7db138ca
Benjamin Shostak <benshostak@rocketmail.com>
2020-11-21 20:36:33 -0800

    adding employee obj/running tests

--- employee.js
+++ employee.js
@@ -1,7 +1,17 @@
-// todo:write cide ti define and export the employee class
+// todo:write code to define and export the employee class
+const Employee = require("/employee.js");
 
-class Employee {
-
+class Employee  {
+    constructor(name, id , email, getname){
+        this.name = name;
+        this.id = id;
+        this.email = email;
+        this.getname = function(name) {
+            this.firstname = name;
+        }
+        this.getid = function(id) {
+            this.id = id;
+        }
 }
 
 module.exports = Employee;
